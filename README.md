2e191ce4a80eb5c8af5eaafaf5d8bd3c45557491
dan <danlove99@hotmail.co.uk>
2020-04-17 12:04:29 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Shopper-Intention
+![image](https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.insider.com%2Fbest-time-to-go-grocery-shopping-2018-3&psig=AOvVaw2IT07jA1HjiDXS-0Y11pJV&ust=1587207756561000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMjHwein7-gCFQAAAAAdAAAAABAD)
+
+## About
+
+A deep neural network (TFLearn) trained to decide whether a shopper is likley to spend money or not.
+
---
+++ train.py
@@ -0,0 +1,49 @@
+from numpy import loadtxt, reshape, expand_dims
+import numpy as np
+import tflearn
+from tflearn.layers.core import fully_connected, dropout, input_data
+from tflearn.layers.estimator import regression
+import pandas as pd
+from sklearn import preprocessing
+from  sklearn.model_selection import train_test_split
+import urllib.request
+
+# load the dataset
+url = 'online_shoppers_intention.csv'
+df = pd.read_csv(url)
+y = df['Revenue'].values
+y.shape = (12330, 1)
+
+
+X = df.drop(['Revenue'], axis=1)
+print(X.shape)
+#X.fillna(0)
+
+
+X =((X-X.min())/(X.max()-X.min()))*20
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+X = expand_dims(X_train, axis=2)
+X = np.nan_to_num(X)
+
+#y = expand_dims(y_train, axis=2)
+
+nn = input_data(shape=[None, 17, 1], name='input')
+nn = fully_connected(nn,16, activation='relu')
+nn = dropout(nn, 0.4)
+nn = fully_connected(nn,32, activation='relu')
+nn = dropout(nn, 0.4)
+nn = fully_connected(nn,64, activation='relu')
+nn = dropout(nn, 0.4)
+nn = fully_connected(nn,32, activation='relu')
+nn = dropout(nn, 0.4)
+nn = fully_connected(nn,16, activation='relu')
+nn = dropout(nn, 0.4)
+nn = fully_connected(nn, 1, activation='sigmoid')
+nn = regression(nn, optimizer='adam', learning_rate=1e-3, loss='binary_crossentropy', name='targets')
+nn = tflearn.DNN(nn, tensorboard_dir='log')
+
+nn.fit(X, y, n_epoch=10, snapshot_step=200, show_metric=True, batch_size=10)
+
+accuracy = nn.evaluate(X, y)
+print('Accuracy: %.2f' % (accuracy[0]*100))
