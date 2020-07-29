e627d49e16d65dadda03cb635ff0402bd72fe22d
Daniel Love <danlove99@hotmail.co.uk>
2020-07-29 19:37:49 +0000

    Cleaned code

--- train.py
+++ train.py
@@ -1,12 +1,12 @@
 from numpy import loadtxt, reshape, expand_dims
 import numpy as np
 import pandas as pd
-from  sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 import urllib.request
 import os 
 
+# Attempt to load numpy array else load the dataset
 try:
-
 	X = np.load('features.npy')
 	y = np.load('labels.npy')
 	print("Succesfully loaded preprocessed features and labels!")
@@ -26,20 +26,20 @@ except:
 # split and reshape data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
-choice = input('k = keras | t = tensorflow \nWhat framework do you want to use: ')
+choice = input('k = Keras | t = TFLearn \nWhat framework do you want to use: ')
+# if using tflearn, make data suitable
 if choice.lower() == 't':
 	X = expand_dims(X_train, axis=2)
 
-
 y.shape = (12330, 1)
-X = np.nan_to_num(X)
+X = np.nan_to_num(X) # Convert all instances of NaN to 0
 
-if choice.lower() == 'k':
+if choice.lower() == 'k': # Keras
 	import kerasclass
 
 	model = kerasclass.model(17)
 	model.train(X,y)
-elif choice.lower() == 't':
+elif choice.lower() == 't': # TFLearn
 	import tflearnclass
 
 	model = tflearnclass.model(17)
