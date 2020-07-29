4acc34567c4fe76124642d79c89b228619eb963e
Daniel Love <danlove99@hotmail.co.uk>
2020-07-29 19:32:44 +0000

    removed too many dropouts for improved model

--- kerasclass.py
+++ kerasclass.py
@@ -6,17 +6,13 @@ class model:
 	def __init__(self, input_size):
 		self.nn = Sequential()
 		self.nn.add(Dense(16, input_dim=input_size, activation='relu'))
-		self.nn.add(Dropout(0.4))
 		self.nn.add(Dense(32, activation='relu'))
 		self.nn.add(Dropout(0.4))
 		self.nn.add(Dense(64, activation='relu'))
-		self.nn.add(Dropout(0.4))
 		self.nn.add(Dense(32, activation='relu'))
-		self.nn.add(Dropout(0.4))
 		self.nn.add(Dense(16, activation='relu'))
 		self.nn.add(Dropout(0.4))
 		self.nn.add(Dense(8, activation='relu'))
-		self.nn.add(Dropout(0.4))
 		self.nn.add(Dense(1, activation='sigmoid'))
 		self.nn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
